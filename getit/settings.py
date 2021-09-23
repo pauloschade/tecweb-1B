e46acd9944873157e855297165a1cdb956e15ca6
pauloschade <paulosc4@al.insper.edu.br>
2021-09-23 16:26:05 -0300

    test

--- getit/settings.py
+++ getit/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'getit',
-        'USER': 'getituser',
-        'PASSWORD': 'getitsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'getit',
+#         'USER': 'getituser',
+#         'PASSWORD': 'getitsenha',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
-# }
+#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
